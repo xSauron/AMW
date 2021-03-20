de6bfbbc446b03af7d283c2a3545393c3361d4af
Adrian Jasek <xSauron@outlook.com>
2021-03-20 17:16:55 +0100

    Implementacja Programu 3

---
+++ Algorytmów i złożoności/18.03.2021/Implementacja Programu 3/Implementacja Programu 3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Implementacja_Programu_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj N = ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            int[] a = new int[N+1];
+
+            int minimalna = 0;
+
+            for(int i = 1; i <= N; i++)
+            {
+                Console.Write("Podaj wartość a[" + i + "] = ");
+                a[i] = Convert.ToInt32(Console.ReadLine());
+
+                if (i == 1 || a[i] < minimalna)
+                {
+                    minimalna = a[i];
+                }
+            }
+
+            Console.WriteLine("Liczba minimalna = " + minimalna);
+            Console.WriteLine("Kliknij dowolny klawisz aby zamknąć program!");
+            Console.ReadKey();
+        }
+    }
+}
