58db7508cf26746d9adb499b6cb9cdb205a81263
Adrian Jasek <xSauron@outlook.com>
2021-03-22 21:29:39 +0100

    Implementacja Programu 4
    1 i 2

---
+++ Algorytmów i złożoności/25.03.2021/Implementacja Programu 4.1/Implementacja Programu 4.1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Implementacja_Programu_4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj N = ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            int[] a = new int[N + 1];
+
+            int H = N;
+            for (int i = 1; i <= N; i++)
+            {
+                Console.Write("Podaj wartość a[" + i + "] = ");
+                a[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+            int s = 1;
+            while (s != 0) {
+                s = 0;
+                for (int i = 1; i < N; i++)
+                    {
+                        Console.WriteLine("Czy " + a[i] + " > " + a[i+1]);
+                        if (a[i] > a[i+1])
+                        {
+                            int t = a[i];
+                            a[i] = a[i + 1];
+                            a[i + 1] = t;
+                            s = s + 1;
+                        }
+                    }
+                N = N - 1;          
+            }
+
+            Console.WriteLine("-----");
+            for (int i = 1; i <= H; i++)
+            {
+                Console.WriteLine("Wartość a[" + i + "] = " + a[i]);
+
+            }
+
+            Console.WriteLine("Kliknij dowolny klawisz aby zamknąć program!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Algorytmów i złożoności/25.03.2021/Implementacja Programu 4.2/Implementacja Programu 4.2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Implementacja_Programu_4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj N = ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            int[] a = new int[N + 1];
+
+            for (int k = 1; k <= N; k++)
+            {
+                Console.Write("Podaj wartość a[" + k + "] = ");
+                a[k] = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+            int s = 1;
+            int i = 1;
+            while (s != 0)
+            {
+                s = 0;
+                
+
+                for (int j = N; j > i; j--)
+                {
+                    Console.WriteLine("Czy " + a[j] + " > " + a[j - 1]);
+                    if (a[j] < a[j - 1])
+                    {
+                        int t = a[j];
+                        a[j] = a[j - 1];
+                        a[j - 1] = t;
+                        s = 1;
+                    }
+                }
+                i++;
+            }
+
+            Console.WriteLine("-----");
+            for (int k = 1; k <= N; k++)
+            {
+                Console.WriteLine("Wartość a[" + i + "] = " + a[i]);
+
+            }
+
+            Console.WriteLine("Kliknij dowolny klawisz aby zamknąć program!");
+            Console.ReadKey();
+        }
+    }
+}
