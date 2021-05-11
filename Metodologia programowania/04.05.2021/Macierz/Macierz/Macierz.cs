0c85d1d34ffafaf322120484112d884ea15c66ac
Adrian Jasek <xSauron@outlook.com>
2021-05-11 13:08:06 +0200

    Update

---
+++ Metodologia programowania/04.05.2021/Macierz/Macierz/Macierz.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Macierz
+{
+    class Macierz
+    {
+        public string nazwa;
+        int liczba_w, liczba_k;
+        int[,] macierz;
+
+        public Macierz()
+        {
+            Console.Write("Podaj nazwe macierzy = ");
+            nazwa = Console.ReadLine();
+            Console.Write("Podaj ilosc wierszy macierzy = ");
+            liczba_w = int.Parse(Console.ReadLine());
+            Console.Write("Podaj ilosc kolumn macierzy = ");
+            liczba_k = int.Parse(Console.ReadLine());
+
+            macierz = new int[liczba_w, liczba_k];
+
+        }
+            
+        public void wyswietl()
+        {
+            Console.WriteLine(this.nazwa + ":");
+            for (int w = 0; w < this.liczba_w; w++)
+            {
+                for (int k = 0; k < this.liczba_k; k++)
+                {
+                    Console.Write(this.macierz[w, k].ToString() + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void wypelnijLosowymi()
+        {
+            Random random = new Random();
+            for (int w = 0; w < this.liczba_w; w++)
+            {
+                for (int k = 0; k < this.liczba_k; k++)
+                {
+                    
+                    this.macierz[w, k] = random.Next(-100,100);
+                }
+                
+            }
+            Console.WriteLine("Wypelniono!");
+        }
+
+        public void wypelnij()
+        {
+            Random random = new Random();
+            for (int w = 0; w < this.liczba_w; w++)
+            {
+                for (int k = 0; k < this.liczba_k; k++)
+                {
+                    Console.Write("Podaj [" + w + "," + k + "] = ");
+
+                    this.macierz[w, k] = int.Parse(Console.ReadLine());
+                }
+
+            }
+            Console.WriteLine("Wypelniono!");
+        }
+
+        public void dodaj(Macierz B)
+        {
+            Console.WriteLine();
+            this.wyswietl();
+            Console.WriteLine();
+            B.wyswietl();
+
+            if (this.liczba_w != B.liczba_w || this.liczba_k != B.liczba_k) { 
+                Console.WriteLine("\n_!_ Ilosc wierszy i kolumn w obu macierzach musi się zgadzać _!_");
+                return;
+            }
+
+            Console.WriteLine("\nSuma: ");
+
+            for (int w = 0; w < this.liczba_w; w++)
+            {
+                for (int k = 0; k < this.liczba_k; k++)
+                {
+                    Console.Write((this.macierz[w, k] + B.macierz[w, k]).ToString() + " ");
+                }
+                    Console.WriteLine();
+            }
+        }
+        
+        public void odejmij(Macierz B)
+        {
+            Console.WriteLine();
+            this.wyswietl();
+            Console.WriteLine();
+            B.wyswietl();
+
+            if (this.liczba_w != B.liczba_w || this.liczba_k != B.liczba_k)
+            {
+                Console.WriteLine("\n_!_ Ilosc wierszy i kolumn w obu macierzach musi się zgadzać _!_");
+                return;
+            }
+
+            Console.WriteLine("\nRóznica: ");
+
+            for (int w = 0; w < this.liczba_w; w++)
+            {
+                for (int k = 0; k < this.liczba_k; k++)
+                {
+                    Console.Write((this.macierz[w, k] + B.macierz[w, k]).ToString() + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public  void skalar(int skalar)
+        {
+            Console.WriteLine();
+            this.wyswietl();
+            Console.WriteLine("\nMnożenie przez skalar: ");
+            for (int w = 0; w < this.liczba_w; w++)
+            {
+                for (int k = 0; k < this.liczba_k; k++)
+                {
+                    Console.Write((this.macierz[w, k] * skalar).ToString() + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        
+        public void pomnoz(Macierz B)
+        {
+            if (this.liczba_w != B.liczba_k)
+            {
+                Console.WriteLine("\n_!_ Ilosc wierszy macierzy 1 musi się równać liczbie kolumn w 2 _!_");
+                return;
+            }
+
+            Console.WriteLine();
+            this.wyswietl();
+            Console.WriteLine();
+            B.wyswietl();
+
+            Console.WriteLine("\nMnożenie przez macierz: ");
+
+            int suma = 0;
+            for (int w = 0; w < this.liczba_w; w++)
+            {
+                
+                for (int k = 0; k < B.liczba_k; k++)
+                {
+                    suma = 0;
+                    for (int i = 0; i < this.liczba_k; i++)
+                    {
+                        //Console.WriteLine(this.macierz[w, i] + " * " + B.macierz[i, k]);
+                        suma += this.macierz[w, i] * B.macierz[i, k];
+                    }
+                        
+                    Console.Write(suma + " ");
+                    //macierzA[i, 0] * macierzB[0, i];
+                }
+                
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
---
+++ Metodologia programowania/04.05.2021/Macierz/Macierz/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Macierz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static public void Instrukcja()
+        {
+            Console.WriteLine(".-----| Wybierz działanie |-----.");
+            Console.WriteLine("|                               |");
+            Console.WriteLine("| 1 - Dodaj nowa macierz        |");
+            Console.WriteLine("| 2 - Wypelnij macierz          |");
+            Console.WriteLine("| 3 - Wypelnij losowymi         |");
+            Console.WriteLine("| 4 - Wyświetl macierz          |");
+            Console.WriteLine("| 5 - Kalkulator                |");
+            Console.WriteLine("| k - Koniec                    |");
+            Console.WriteLine("|                               |");
+            Console.WriteLine("*-------------------------------*");
+        }
+
+        static public void Menu()
+        {
+            bool koniec = false;
+
+            Dictionary<string, Macierz> Macierze = new Dictionary<string, Macierz>();
+            string nazwa;
+            do
+            {
+                Instrukcja();
+
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.D1:
+                        var macierz = new Macierz();
+                        Macierze[macierz.nazwa] = macierz;
+                        break;
+                    case ConsoleKey.D2:
+                        szukaj(Macierze, "0").wypelnij();
+                        break;
+                    case ConsoleKey.D3:
+                        szukaj(Macierze, "0").wypelnijLosowymi();
+                        break;
+                    case ConsoleKey.D4:
+                        szukaj(Macierze, "0").wyswietl();
+                        break;
+                    case ConsoleKey.D5:
+                        operacje(Macierze);
+                        break;
+                    case ConsoleKey.K:
+                        koniec = true;
+                        return;
+                    default:
+                        Console.WriteLine("___Wybierz opcje z menu!___");
+                        break;
+                }
+
+
+
+                Console.WriteLine("\n_>_ Wciśnij dowolny przycisk aby kontynuować _<_");
+                Console.ReadKey();
+                Console.Clear();
+            } while (koniec == false);
+        }
+
+        public static void operacje(Dictionary<string, Macierz> Macierze)
+        {
+            Console.Write("Podaj operacje np. A + B, A * B, A - B, A * 3: \n");
+            string operacja = Console.ReadLine();
+            string[] op = operacja.Split(' ');
+
+            switch(op[1]){
+                case "+":
+                    szukaj(Macierze, op[0].ToString()).dodaj(szukaj(Macierze,op[2].ToString()));
+                    break;
+                case "-":
+                    szukaj(Macierze, op[0].ToString()).odejmij(szukaj(Macierze, op[2].ToString()));
+                    break;
+                case "*":
+                    int sk;
+                    char B;
+
+                    if (int.TryParse(op[2],out sk))
+                    {
+                        szukaj(Macierze, op[0].ToString()).skalar(sk);
+                    }
+                    else
+                    {
+                        szukaj(Macierze, op[0].ToString()).pomnoz(szukaj(Macierze, op[2].ToString()));
+                    }
+                    break;
+            }
+        }
+
+        public static Macierz szukaj(Dictionary<string, Macierz> Macierze, string nazwa)
+        {
+            if(nazwa == "0") { 
+                Console.Write("Podaj nazwe macierzy = ");
+                string wczytaj = Console.ReadLine();
+                if (Macierze.TryGetValue(wczytaj, out var macierz))
+                    return macierz;
+                else if (wczytaj == "k")
+                    return null;
+                else
+                    return szukaj(Macierze, "0");
+            }
+            else
+            {
+                if (Macierze.TryGetValue(nazwa, out var macierz))
+                    return macierz;
+                else
+                    return null;
+            }
+        }
+    }
+}
