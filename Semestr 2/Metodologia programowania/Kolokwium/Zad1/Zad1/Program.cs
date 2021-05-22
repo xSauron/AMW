8ddbec68243b4434ae8fcdc4d53c2b6e0b7bd25c
Adrian Jasek <xSauron@outlook.com>
2021-05-22 22:19:24 +0200

    Kolokwium

---
+++ Semestr 2/Metodologia programowania/Kolokwium/Zad1/Zad1/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+namespace Zad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+        static public void Instrukcja()
+        {
+            Console.WriteLine(".-----| Wybierz działanie |-----.");
+            Console.WriteLine("|                               |");
+            Console.WriteLine("| 1 - Trójkat z boków           |");
+            Console.WriteLine("| 2 - Trójkat z wierzchołków    |");
+            Console.WriteLine("| k - Koniec                    |");
+            Console.WriteLine("|                               |");
+            Console.WriteLine("*-------------------------------*");
+        }
+
+        static public void Menu()
+        {
+            bool koniec = false;
+
+            do
+            {
+                Instrukcja();
+                Trojkat trojkat;
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.D1:
+                        trojkat = new TrBoki();
+                        Console.WriteLine("Pole: " + trojkat.pole() + "\nObwód: " + trojkat.obwod());
+                        break;
+                    case ConsoleKey.D2:
+                        trojkat = new TrWierzch();
+                        Console.WriteLine("Pole: " + trojkat.pole() + "\nObwód: " + trojkat.obwod());
+                        break;
+                    case ConsoleKey.K:
+                        koniec = true;
+                        return;
+                    default:
+                        Console.WriteLine("_!_ Wybierz opcje z menu _!_");
+                        break;
+                }
+
+                Console.WriteLine("\n_>_ Wciśnij dowolny przycisk aby kontynuować _<_");
+                Console.ReadKey();
+                Console.Clear();
+            } while (koniec == false);
+        }
+    }
+
+    abstract class Trojkat
+    {
+
+        public abstract double obwod();
+
+        public abstract double pole();
+
+    }
+
+    class TrBoki : Trojkat
+    {
+        double a, b, c;
+
+        public TrBoki(){
+            Console.Write("Podaj a= ");
+            a = double.Parse(Console.ReadLine());
+            Console.Write("Podaj b= ");
+            b = double.Parse(Console.ReadLine());
+            Console.Write("Podaj c= ");
+            c = double.Parse(Console.ReadLine());
+        }
+
+        public override double obwod()
+        {
+            return a + b + c;
+        }
+
+        public override double pole()
+        {
+            double s = 0.5f * (a + b + c);
+            double p = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+
+            //Sprawdzamy czy da się obliczyć
+            if (double.IsNaN(p))
+                return 0;
+            else
+                return p;
+        }
+    }
+    class TrWierzch : Trojkat
+    {
+        double x1,x2,x3,y1,y2,y3;
+
+        public TrWierzch()
+        {
+            Console.Write("Podaj x1= ");
+            x1 = double.Parse(Console.ReadLine());
+            Console.Write("Podaj y1= ");
+            y1 = double.Parse(Console.ReadLine());
+            Console.Write("Podaj x2= ");
+            x2 = double.Parse(Console.ReadLine());
+            Console.Write("Podaj y2= ");
+            y2 = double.Parse(Console.ReadLine());
+            Console.Write("Podaj x3= ");
+            x3 = double.Parse(Console.ReadLine());
+            Console.Write("Podaj y3= ");
+            y3 = double.Parse(Console.ReadLine());
+        }
+
+        public override double obwod()
+        {
+            double p1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            double p2 = Math.Sqrt(Math.Pow(x3 - x2, 2) + Math.Pow(y3 - y2, 2));
+            double p3 = Math.Sqrt(Math.Pow(x1 - x3, 2) + Math.Pow(y1 - y3, 2));
+            return p1 + p2 + p3;
+        }
+
+        public override double pole()
+        {
+            //Poprawione brakowało jednego nawiasu w poleceniu - Bledna kolejnosc wykonywania
+            double p = Math.Abs((0.5 * ((x1 * y2 - x2 * y1) + (x2 * y3 - x3 * y2) + (x3 * y1 - x1 * y3))));
+
+            return p;
+        }
+    }
+
+}
---
+++ Semestr 2/Metodologia programowania/Kolokwium/Zad2/Zad2/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+
+namespace Zad2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+        static public void Instrukcja()
+        {
+            Console.WriteLine(".-----| Wybierz działanie |-----.");
+            Console.WriteLine("|                               |");
+            Console.WriteLine("| 1 - Stworz tablice            |");
+            Console.WriteLine("| 2 - Wczytaj dane do tablicy   |");
+            Console.WriteLine("| 3 - Wczytaj losowe do tablicy |");
+            Console.WriteLine("| 4 - Wyswietl max              |");
+            Console.WriteLine("| 5 - Wyswietl sumy             |");
+            Console.WriteLine("| k - Koniec                    |");
+            Console.WriteLine("|                               |");
+            Console.WriteLine("*-------------------------------*");
+        }
+
+        static public void Menu()
+        {
+            bool koniec = false;
+            Tablica tablica = null;
+            do
+            {
+                Instrukcja();
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.D1:
+                        tablica = new Tablica();
+                        break;
+                    case ConsoleKey.D2:
+                        tablica.wypelnij();
+                        break;
+                    case ConsoleKey.D3:
+                        tablica.wypelnijLosowymi();
+                        break;
+                    case ConsoleKey.D4:
+                        tablica.wyswietl(false);
+                        break;
+                    case ConsoleKey.D5:
+                        tablica.wyswietl(true);
+                        break;
+                    case ConsoleKey.K:
+                        koniec = true;
+                        return;
+                    default:
+                        Console.WriteLine("_!_ Wybierz opcje z menu _!_");
+                        break;
+                }
+
+                Console.WriteLine("\n_>_ Wciśnij dowolny przycisk aby kontynuować _<_");
+                Console.ReadKey();
+                Console.Clear();
+            } while (koniec == false);
+        }
+    }
+
+    class Tablica
+    {
+        int[,] tablica;
+        int liczba_w, liczba_k;
+
+        public Tablica()
+        {
+            Console.Write("Podaj ilosc wierszy tablicy = ");
+            liczba_w = int.Parse(Console.ReadLine());
+            Console.Write("Podaj ilosc kolumn tablicy = ");
+            liczba_k = int.Parse(Console.ReadLine());
+
+            tablica = new int[liczba_w, liczba_k];
+        }
+
+        public void wyswietl(bool suma)
+        {
+            if (suma) {
+                Console.WriteLine("Suma:");
+            }
+            else
+            {
+                Console.WriteLine("Max:");
+            }
+
+
+            for (int w = 0; w < this.liczba_w; w++)
+            {
+
+                for (int k = 0; k < this.liczba_k; k++)
+                {
+                    Console.Write(this.tablica[w, k].ToString() + " ");
+
+                }
+                Console.WriteLine();
+            }
+
+            int max, s;
+            for (int k = 0; k < this.liczba_k; k++)
+            {
+                max = int.MinValue;
+                s = 0;
+                for (int w = 0; w < this.liczba_w; w++)
+                {
+                    s += this.tablica[w, k];
+                    if (this.tablica[w, k] > max)
+                    {
+                        max = this.tablica[w, k];
+                    }
+                }
+                if (suma)
+                {
+                    Console.Write(s + " ");
+                }
+                else
+                {
+                    Console.Write(max + " ");
+                }
+                
+            }
+        }
+
+        public void wypelnijLosowymi()
+        {
+            Random random = new Random();
+            for (int w = 0; w < this.liczba_w; w++)
+            {
+                for (int k = 0; k < this.liczba_k; k++)
+                {
+
+                    this.tablica[w, k] = random.Next(-100, 100);
+                }
+
+            }
+            Console.WriteLine("Wypelniono losowymi!");
+        }
+
+        public void wypelnij()
+        {
+            Random random = new Random();
+            for (int w = 0; w < this.liczba_w; w++)
+            {
+                for (int k = 0; k < this.liczba_k; k++)
+                {
+                    Console.Write("Podaj [" + w + "," + k + "] = ");
+
+                    this.tablica[w, k] = int.Parse(Console.ReadLine());
+                }
+
+            }
+            Console.WriteLine("Wypelniono wlasnymi!");
+        }
+
+
+    }
+
+}
+
